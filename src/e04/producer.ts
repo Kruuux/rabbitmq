5169211fdac5343b95d09a7d9f9c25bf856f0f35
Kruuux <kruczekkonrad.kk@gmail.com>
2022-07-25 19:03:23 +0200

    feat: complete episode 4 task

---
+++ src/e04/producer.ts
@@ -0,0 +1,13 @@
+import amqplib from 'amqplib';
+
+(async () => {
+  const queue = 'tasks';
+  const conn = await amqplib.connect('amqp://localhost');
+
+  const ch1 = await conn.createChannel();
+  await ch1.assertQueue(queue);
+
+  setInterval(() => {
+    ch1.sendToQueue(queue, Buffer.from('something to do'));
+  }, 1000);
+})();
